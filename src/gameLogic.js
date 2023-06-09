735505ab3cfcb1ae4b30da10f35ec037634aedc2
Bradley Reynolds <bsreynolds.busi@gmail.com>
2023-06-09 17:21:44 -0500

    Made gameLogic.js and domManipulation.js more clean and easy to read

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -6,59 +6,60 @@ export default class Logic {
     this.gameover = false;
     this.computerMoves = [];
     this.winner = null;
+    this.playAgain = null;
   }
 
   checkwin() {
-    if (this.player.hits === 17) {
+    if (this.player.hits === 17 || this.computer.hits === 17) {
+      this.winner = this.player.hits === 17 ? "Computer" : "Player";
       this.gameover = true;
-      this.winner = "Computer";
-    } else if (this.computer.hits === 17) {
-      this.gameover = true;
-      this.winner = "Player";
     }
 
     if (this.gameover === true) {
-      const newGameBtn = document.createElement("button");
       document.body.innerHTML = "";
       document.body.innerHTML = `
       <div class="winner-container">
-        <p class='winner-text'>The ${this.winner} wins!</p>
+        <p class='winner-text'>${this.winner} wins!</p>
         <button class="winner-btn">Play Again</button>
       </div>`;
 
-      const playAgain = document.querySelector(".winner-btn");
-      playAgain.addEventListener("click", () => {
+      this.playAgain = document.querySelector(".winner-btn");
+      this.playAgain.addEventListener("click", () => {
         location.reload();
       });
     }
   }
 
+  getRands() {
+    let x = Math.floor(Math.random() * 10);
+    let y = Math.floor(Math.random() * 10);
+    return [x, y];
+  }
+
   computerMove() {
     if (this.gameover === false) {
       const playerSquares = document.querySelectorAll(".player-square");
-      let x = Math.floor(Math.random() * 10);
-      let y = Math.floor(Math.random() * 10);
+      let [x, y] = this.getRands();
+
       if (this.computerMoves.includes(JSON.stringify([x, y]))) {
         this.computerMove();
       } else {
-        if (this.player.board[y][x] === "w") {
+        let hit = this.player.board[y][x] === "placed" ? "hit" : "miss";
+
+        if (hit === "hit") {
           this.player.isHit();
-          this.player.board[y][x] = "i";
-          this.player.board.flat().forEach((ele, i) => {
-            if (ele === "i") {
-              playerSquares[i].style.textContent = "x";
-              playerSquares[i].style.color = "red";
-            }
-          });
-        } else {
-          this.player.board[y][x] = "m";
-          this.player.board.flat().forEach((ele, i) => {
-            if (ele === "m") {
-              playerSquares[i].style.textContent = "o";
-              playerSquares[i].style.color = "black";
-            }
-          });
         }
+
+        this.player.board[y][x] = `${hit}`;
+        this.player.board.flat().forEach((ele, i) => {
+          if (ele === "hit") {
+            playerSquares[i].style.textContent = "x";
+            playerSquares[i].style.color = "red";
+          } else if (ele === "miss") {
+            playerSquares[i].style.textContent = "o";
+            playerSquares[i].style.color = "black";
+          }
+        });
         this.computerMoves.push(JSON.stringify([x, y]));
       }
     }
